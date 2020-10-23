bd9eca927f696a8b232e2aba28986f2e46c9217f
mahinour30 <49105771+mahinour30@users.noreply.github.com>
2020-10-23 06:04:00 +0200

    Add files via upload

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import TodoItem from '../TodoItem';
 
 function TodoList() {
+
+  let todos = useSelector(state=> state)
   return (
     <div>
-      <TodoItem />
-      <TodoItem />
+
+      {/* here we map todos and with every todo we display a todo item and pass the id as its key  */}
+      {todos.map(todo =>{
+        return<TodoItem 
+        key = {todo.id} 
+        todo={todo}
+        />
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexPage from './pages/index';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
+
+
 
 ReactDOM.render(
-    <React.StrictMode>
+  <React.StrictMode>
+    {/* used provider to access the store from any component in the application */}
+    <Provider store={store}>
       <IndexPage />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </Provider>
+  </React.StrictMode>
+    
+    ,document.getElementById('root')
 );
